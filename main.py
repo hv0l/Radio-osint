50a8ea2fd8e0dcced97c1ee511ba58f7dc104b2f
hv90 <61795418+hv0l@users.noreply.github.com>
2024-02-12 16:12:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import gnuradio_connector
-import audio_processor
 import speech_to_text
+from convert_audio import raw_to_wav 
 
 def main():
     frequency = 100.0e6  
-    duration = 10
-    
+    duration = 10 
+    raw_audio_path = "output.raw"
+    wav_audio_path = "output.wav"
+
     print(f"Connessione alla frequenza {frequency} Hz per {duration} secondi...")
     audio_path = gnuradio_connector.connect_and_record(frequency, duration)
     
     if audio_path:
+        print("Conversione dell'audio in formato WAV...")
+        raw_to_wav(raw_audio_path, wav_audio_path)
+        
         print("Trasformazione dell'audio in testo...")
-        text = speech_to_text.audio_to_text(audio_path)
+        text = speech_to_text.audio_to_text(wav_audio_path)
         
         print("Trascrizione completata:")
         print(text)
